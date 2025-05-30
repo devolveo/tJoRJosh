80befe960c99dec9e2e2821451dd6b489e9968e2
Dluthfi <thedevolveo@gmail.com>
2025-05-31 04:22:01 +0700

    modal-add features

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,56 @@ function App() {
         Ipsum.
       </p>
       <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        voluptatum magni iusto? Dolorem pariatur totam voluptate deleniti
+        aliquam, quos iusto suscipit qui, sapiente quis est nihil enim molestiae
+        autem magni.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+        deserunt magnam nulla. Soluta nihil nam aspernatur nisi quibusdam,
+        veniam tempora natus dicta adipisci, nobis error, fuga animi pariatur
+        quam obcaecati?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        maxime amet mollitia cum rerum dolores minus temporibus. Sunt
+        repudiandae quae cupiditate totam adipisci! Accusamus minus iure
+        laudantium assumenda veritatis repudiandae!
+      </p>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </p>
+      <p>
+        It was popularised in the 1960s with the release of Letraset sheets
+        containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        voluptatum magni iusto? Dolorem pariatur totam voluptate deleniti
+        aliquam, quos iusto suscipit qui, sapiente quis est nihil enim molestiae
+        autem magni.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+        deserunt magnam nulla. Soluta nihil nam aspernatur nisi quibusdam,
+        veniam tempora natus dicta adipisci, nobis error, fuga animi pariatur
+        quam obcaecati?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        maxime amet mollitia cum rerum dolores minus temporibus. Sunt
+        repudiandae quae cupiditate totam adipisci! Accusamus minus iure
+        laudantium assumenda veritatis repudiandae!
+      </p>
+      <p>
         <a href="https://www.lipsum.com/">Read more on lipsum.com</a>.
       </p>
     </>
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,19 +1,53 @@
 import React from "react";
+import FocusLock from "react-focus-lock";
+import { RemoveScroll } from "react-remove-scroll";
+import VisuallyHidden from "./visuallyHidden.jsx";
+
 import { X as Close } from "react-feather";
 
 import styles from "./Modal.module.css";
 
 function Modal({ handleDismiss, children }) {
+  const closeBtnRef = React.useRef();
+  React.useEffect(() => {
+    const currFocusElem = document.activeElement;
+    closeBtnRef.current.focus();
+    return () => {
+      currFocusElem?.focus();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    function handleDismissByEscape(event) {
+      if (event.key === "Escape") {
+        handleDismiss();
+      }
+    }
+
+    document.addEventListener("keydown", handleDismissByEscape);
+    return () => {
+      document.removeEventListener("keydown", handleDismissByEscape);
+    };
+  }, [handleDismiss]);
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.backdrop} />
-      <div className={styles.dialog}>
-        <div className={styles.closeBtn} onClick={handleDismiss}>
-          <Close />
+    <FocusLock>
+      <RemoveScroll>
+        <div className={styles.wrapper}>
+          <div className={styles.backdrop} onClick={handleDismiss} />
+          <div className={styles.dialog}>
+            <button
+              ref={closeBtnRef}
+              className={styles.closeBtn}
+              onClick={handleDismiss}
+            >
+              <Close />
+              <VisuallyHidden>Dismiss modal</VisuallyHidden>
+            </button>
+            {children}
+          </div>
         </div>
-        {children}
-      </div>
-    </div>
+      </RemoveScroll>
+    </FocusLock>
   );
 }
 
