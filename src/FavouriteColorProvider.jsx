b382a3fff741d4477e70d5f970d720511f640600
Dluthfi <thedevolveo@gmail.com>
2025-05-30 16:36:44 +0700

    colorpicker final

--- src/FavouriteColorProvider.jsx
+++ src/FavouriteColorProvider.jsx
@@ -5,10 +5,12 @@ export const FavouriteColorContext = React.createContext();
 function FavouriteColorProvider({ children }) {
   const [favouriteColor, setFavouriteColor] = React.useState("#EBDEFB");
 
+  const value = React.useMemo(() => {
+    return { favouriteColor, setFavouriteColor };
+  }, [favouriteColor]);
+
   return (
-    <FavouriteColorContext.Provider
-      value={{ favouriteColor, setFavouriteColor }}
-    >
+    <FavouriteColorContext.Provider value={value}>
       {children}
     </FavouriteColorContext.Provider>
   );
