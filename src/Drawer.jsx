fdad8912ad41dac941a58941e6c63c27e97eeb78
Dluthfi <thedevolveo@gmail.com>
2025-06-01 03:52:35 +0700

    modal exercise finished

--- src/Drawer.jsx
+++ src/Drawer.jsx
@@ -6,16 +6,47 @@ import { RemoveScroll } from "react-remove-scroll";
 import styles from "./Drawer.module.css";
 
 function Drawer({ handleDismiss, children }) {
+  const btnDismissRef = React.useRef(null);
+  React.useEffect(() => {
+    const prevActiveEl = document.activeElement;
+    // btnDismissRef.current.focus();
+    return () => {
+      prevActiveEl?.focus();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === "Escape") {
+        handleDismiss();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleDismiss]);
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.backdrop} />
-      <div className={styles.drawer}>
-        <div>{children}</div>
-        <button className={styles.closeBtn} onClick={handleDismiss}>
-          <Close size={18} /> Dismiss
-        </button>
-      </div>
-    </div>
+    <FocusLock>
+      <RemoveScroll>
+        <div className={styles.wrapper}>
+          <div className={styles.backdrop} onClick={handleDismiss} />
+          <div className={styles.drawer}>
+            <div>{children}</div>
+            <button
+              ref={btnDismissRef}
+              className={styles.closeBtn}
+              onClick={handleDismiss}
+            >
+              <Close size={18} /> Dismiss
+            </button>
+          </div>
+        </div>
+      </RemoveScroll>
+    </FocusLock>
   );
 }
 
