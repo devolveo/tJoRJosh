394527849998d54884c485e0a7c960a4357a11fd
Dluthfi <thedevolveo@gmail.com>
2025-06-01 09:08:48 +0700

    modal exercise2 finished

--- src/Drawer.jsx
+++ src/Drawer.jsx
@@ -6,17 +6,36 @@ import { RemoveScroll } from "react-remove-scroll";
 import styles from "./Drawer.module.css";
 
 function Drawer({ handleDismiss, children }) {
+  useEscKey(handleDismiss);
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.backdrop} />
-      <div className={styles.drawer}>
-        <div>{children}</div>
-        <button className={styles.closeBtn} onClick={handleDismiss}>
-          <Close size={18} /> Dismiss
-        </button>
-      </div>
-    </div>
+    <FocusLock returnFocus={true}>
+      <RemoveScroll>
+        <div className={styles.wrapper}>
+          <div className={styles.backdrop} onClick={handleDismiss} />
+          <div className={styles.drawer}>
+            <div>{children}</div>
+            <button className={styles.closeBtn} onClick={handleDismiss}>
+              <Close size={18} /> Dismiss
+            </button>
+          </div>
+        </div>
+      </RemoveScroll>
+    </FocusLock>
   );
+}
+
+function useEscKey(callback) {
+  React.useEffect(() => {
+    function handleEscKey(e) {
+      if (e.key === "Escape") {
+        callback();
+      }
+    }
+    window.addEventListener("keydown", handleEscKey);
+    return () => {
+      window.removeEventListener("keydown", handleEscKey);
+    };
+  }, [callback]);
 }
 
 export default Drawer;
